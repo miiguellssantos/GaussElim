37408704ca2d15784cb05b468a318f6aae627a79
miiguellsantos <lordellomiguel9@gmail.com>
2026-09-07 13:13:00 -0300

    cleanup: removing unused code in main.c

--- main.c
+++ main.c
@@ -6,46 +6,17 @@
 
 int main() 
 {
-    setlocale(LC_ALL, "");
+    setlocale(LC_ALL, ""); // incluir acentos no terminal (ARRUMAR)
     int equacoes = 0;
 
     printf("Quantas equações serão digitadas? ");
-    if (scanf("%d", &equacoes) == 1 && equacoes > 0)
-    {
+    if (scanf("%d", &equacoes) == 1 && equacoes > 0){
         // Limpa o '\n' restante do buffer do scanf antes de usar fgets
         int c;
-        while ((c = getchar()) != '\n' && c != EOF);
+        while ((c = getchar()) != '\n' && c != EOF); 
 
         scan_equacao(equacoes);
     }
 
     return 0;
 }
-
-/*
-int main() {
-    setlocale(LC_ALL, "");
-    int quantidade = 0;
-    int equacoes = 0;
-
-    printf("Quantas equações serão digitadas? ");
-    scanf("%d", &equacoes);
-
-    scan_equacao(equacoes, &quantidade);
-    
-    return 0;
-} */
-
-/*
-char equacao[] = "12x + 52y - 3 = 0";
-
-int *numeros = identifica_numeros(equacao, &quantidade);
-
-
-printf("Números encontrados: ");
-for (int i = 0; i < quantidade; i++) {
-    printf("%d ", numeros[i]);
-}
-printf("\n");
-free(numeros);
-*/
