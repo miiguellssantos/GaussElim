ba895def253e7b04b0b16d37470e6fd538a40b82
miiguellsantos <lordellomiguel9@gmail.com>
2026-09-13 13:11:51 -0300

    feat(main): allowing program to have multiple equations

--- main.c
+++ main.c
@@ -5,23 +5,48 @@
 #include <locale.h>
 #include <string.h>
 
-int main() 
+int main()
 {
     setlocale(LC_ALL, ""); // incluir acentos no terminal (ARRUMAR)
-    double matriz[3][3];
     int equacoes = 0;
 
-    /*
     printf("Quantas equações serão digitadas? ");
-    if (scanf("%d", &equacoes) == 1 && equacoes > 0){
+    if (scanf("%d", &equacoes) == 1 && equacoes > 0)
+    {
         int c;
-        while ((c = getchar()) != '\n' && c != EOF); 
+        while ((c = getchar()) != '\n' && c != EOF)
+            ;
 
-        scan_equacao(equacoes);
+        // 1. ALOCAÇÃO DINÂMICA
+        // Aloca o array de ponteiros para as linhas
+        double **matriz = (double **)malloc(equacoes * sizeof(double *));
+        if (matriz == NULL)
+        {
+            printf("Erro ao alocar memória!\n");
+            return 1;
+        }
+
+        // Aloca cada linha com a quantidade de colunas (ex: equacoes + 1 se tiver termo independente)
+        int colunas = equacoes; // ou equacoes + 1
+        for (int i = 0; i < equacoes; i++)
+        {
+            matriz[i] = (double *)malloc(colunas * sizeof(double));
+            if (matriz[i] == NULL)
+            {
+                printf("Erro ao alocar memória para a linha %d!\n", i);
+                return 1;
+            }
+        }
+
+        scan_equacao(equacoes, matriz);
+
+        // 3. LIBERAÇÃO DE MEMÓRIA (Obrigatório)
+        for (int i = 0; i < equacoes; i++)
+        {
+            free(matriz[i]); // Libera cada linha
+        }
+        free(matriz); // Libera o array principal de ponteiros
     }
-    */
-
-    scan_equacao(3, matriz);
 
     return 0;
 }
